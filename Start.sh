c84d2180ad7e98482ace7ff625005bb70580d91a
q2533q <q2533q@gmail.com>
2022-08-02 17:47:15 +0800

    Add files via upload

--- Start.sh
+++ Start.sh
@@ -1,45 +1,5 @@
 #!/bin/bash
 
-cat > "/app/config.conf" <<-\EOF
-{
-	"inbounds": [
-		{
-			"port": ${PORT},
-			"protocol": "vmess",
-			"settings": {
-				"clients": [
-					{
-						"id": "88888888-8888-8888-8888-888888888888",
-						"alterId": 0
-					}
-				],
-				"disableInsecureEncryption": false
-			},
-			"streamSettings": {
-				"network": "ws",
-				"security": "none",
-				"wsSettings": {
-					"acceptProxyProtocol": false,
-					"path": "/",
-					"headers": {}
-				}
-			},
-			"sniffing": {
-				"enabled": true,
-				"destOverride": [
-					"http",
-					"tls"
-				]
-			}
-		}
-	],
-	"outbounds": [
-		{
-			"protocol": "freedom",
-			"settings": {}
-		}
-	]
-}
-EOF
-
+sed -i "s/PORT/${PORT}/g" /app/config.json
+chmod -R 777 /app
 /app/x
